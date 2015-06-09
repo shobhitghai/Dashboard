c5bcd232c50364a22cd85cdd3c7b765bf0810737
shobhit <mail.shobhitghai@gmail.com>
2015-06-09 17:45:24 +0530

    changes for tile section layout

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css/build"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "css/img"
 javascripts_dir = "javascripts"
