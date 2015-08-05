92bb8d357333bd4c27027cc8ccad2ffd083f522a
shobhit ghai <mail.shobhitghai@gmail.com>
2015-08-06 01:17:54 +0530

    changes for init auth part

--- dataConnection.js
+++ dataConnection.js
@@ -4,6 +4,7 @@ var application_root = __dirname,
     bodyParser = require("body-parser"),
     dataController = require("./private/controllers/dataController.js"),
     constants = require("./private/constants.js"),
+    session = require('express-session'),
     path = require('path'),
     port = 3000;
 
@@ -37,6 +38,7 @@ DataConnectionLayer.prototype.connectDB = function() {
 
 DataConnectionLayer.prototype.configureExpress = function(connection) {
     var router = express.Router();
+    var sess;
 
     app.use(bodyParser.urlencoded({
         extended: true
@@ -44,13 +46,56 @@ DataConnectionLayer.prototype.configureExpress = function(connection) {
     app.use(bodyParser.json());
     app.use(express.static('public'));
     app.use('/api', router);
-    app.get('/app', function(req, res) {
-        res.sendFile(path.join(__dirname + '/views/index.html'));
-    });
+
+    this._authModule();
 
     var apiController = new dataController(router, connection);
 
     this.startServer();
+}
+
+DataConnectionLayer.prototype._authModule = function() {
+    app.use(session({
+        secret: 'Sess10nSecret',
+        name: 'acton_ops',
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    app.get('/', function(req, res, next) {
+        if (req.session.isLoggedin) {
+            //send to get auth token
+
+            console.log('auth user');
+            res.redirect('/auth_token');
+        } else {
+            //Redirect to login url
+
+            console.log('unauth user')
+
+            //test
+            res.redirect('/app');
+        }
+    });
+
+    app.get('/auth_token', function(req, res) {
+        //send call to other url to get auth token details
+        //http://52.74.64.83/crosslink_auth/oauth/access_token
+        //get access token
+        //success callback
+        //res.redirect('/app')
+
+    });
+
+    app.get('/app', function(req, res) {
+        //get user info by passing token
+        //http://52.74.64.83/crosslink_auth/public/api/user?access_token=PS0WysKrSP0iNNl9nG3gbv4Dv0nxeXcURsTnvkxO
+
+        // if (req.session.isLoggedin) {
+        res.sendFile(path.join(__dirname + '/views/index.html'));
+        // }
+    });
+
 }
 
 DataConnectionLayer.prototype.startServer = function() {
